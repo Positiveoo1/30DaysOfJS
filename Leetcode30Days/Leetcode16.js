1d28a3a2059c69560b20c0bf7817b57fbbf8cf3b
Positiveoo1 <github0512@gmail.com>
2024-11-01 08:36:14 +0100

    easy

---
+++ Leetcode30Days/Leetcode16.js
@@ -0,0 +1,9 @@
+//171. Excel Sheet Column Number EASY
+function titleToNumber(columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++) {
+        const charValue = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        result = result * 26 + charValue;
+    }
+    return result;
+}
