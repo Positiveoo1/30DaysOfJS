34411d22cc0c58352c9f7155c5585111eddf4f53
Positiveoo1 <github0512@gmail.com>
2024-11-12 07:06:42 +0100

    easy

---
+++ Leetcode30Days/Leetcode24.js
@@ -0,0 +1,16 @@
+//3340. Check Balanced String  EASY
+var isBalanced = function(num) {
+    let evenSum = 0;
+    let oddSum = 0;
+    
+    for (let i = 0; i < num.length; i++) {
+        const digit = parseInt(num[i], 10);  
+        if (i % 2 === 0) {
+            evenSum += digit; 
+        } else {
+            oddSum += digit;   
+        }
+    }
+    
+    return evenSum === oddSum;
+};
